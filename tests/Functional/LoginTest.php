0899445c89bc65faab4a4217824317378c122ff3
TBoileau <t-boileau@email.com>
2021-02-01 18:54:38 +0100

    test: authentication

---
+++ tests/Functional/LoginTest.php
@@ -0,0 +1,92 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class LoginTest extends WebTestCase
+{
+    public function testIfLoginIsSuccessful()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request("GET", "/login");
+
+        $form = $crawler->filter("form[name=login]")->form([
+            "email" => "user@email.com",
+            "password" => "password"
+        ]);
+
+        $client->submit($form);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        $client->followRedirect();
+
+        $this->assertRouteSame("index");
+    }
+
+    public function testIfEmailDoesNotExist()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request("GET", "/login");
+
+        $form = $crawler->filter("form[name=login]")->form([
+            "email" => "fail@email.com",
+            "password" => "password"
+        ]);
+
+        $client->submit($form);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        $client->followRedirect();
+
+        $this->assertRouteSame("security_login");
+    }
+
+    public function testIfPasswordIsWrong()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request("GET", "/login");
+
+        $form = $crawler->filter("form[name=login]")->form([
+            "email" => "user@email.com",
+            "password" => "fail"
+        ]);
+
+        $client->submit($form);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        $client->followRedirect();
+
+        $this->assertRouteSame("security_login");
+    }
+
+    public function testIfCsrfIsWrong()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request("GET", "/login");
+
+        $form = $crawler->filter("form[name=login]")->form([
+            "email" => "user@email.com",
+            "password" => "password",
+            "_csrf_token" => "fail"
+        ]);
+
+        $client->submit($form);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        $client->followRedirect();
+
+        $this->assertRouteSame("security_login");
+    }
+}
