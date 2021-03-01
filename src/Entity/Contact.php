0d353f3ebc014e09782792ea1b1d325bc85bdef5
TBoileau <t-boileau@email.com>
2021-03-01 19:49:21 +0100

    feat: contact (#84)

---
+++ src/Entity/Contact.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Exception;
+use Symfony\Component\Validator\Constraints as Assert;
+
+class Contact implements \Serializable
+{
+    /**
+     * @Assert\NotBlank
+     */
+    public string $name;
+
+    /**
+     * @Assert\NotBlank
+     * @Assert\Email
+     */
+    public string $email;
+
+    /**
+     * @Assert\NotBlank
+     */
+    public string $subject;
+
+    /**
+     * @Assert\NotBlank
+     */
+    public string $content;
+
+    public function serialize(): string
+    {
+        return \serialize([
+            "name" => $this->name,
+            "email" => $this->email,
+            "subject" => $this->subject,
+            "content" => $this->content
+        ]);
+    }
+
+    public function unserialize($serialized): void
+    {
+        [
+            "name" => $this->name,
+            "email" => $this->email,
+            "subject" => $this->subject,
+            "content" => $this->content
+        ] = \unserialize($serialized);
+    }
+}
