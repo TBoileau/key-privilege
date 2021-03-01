003325545cf7e707a51be3af3208c47a56360d0b
TBoileau <t-boileau@email.com>
2021-03-01 02:57:48 +0100

    test: manage members (#65)

---
+++ tests/Functional/Admin/MemberTest.php
@@ -0,0 +1,105 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests\Functional\Admin;
+
+use App\Controller\Admin\AdministratorCrudController;
+use App\Controller\Admin\MemberCrudController;
+use App\Entity\Administrator;
+use App\Entity\Company\Member;
+use Doctrine\ORM\EntityManagerInterface;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class MemberTest extends WebTestCase
+{
+    public function testIfMemberManageIsSuccessful(): void
+    {
+        $client = static::createClient();
+
+        /** @var EntityManagerInterface $entityManager */
+        $entityManager = $client->getContainer()->get("doctrine.orm.entity_manager");
+
+        /** @var AdminUrlGenerator $urlGenerator */
+        $adminUrlGenerator = $client->getContainer()->get(AdminUrlGenerator::class);
+
+        $admin = $entityManager->find(Administrator::class, 1);
+
+        $client->loginUser($admin, "admin");
+
+        $client->request(
+            "GET",
+            $adminUrlGenerator
+                ->setController(MemberCrudController::class)
+                ->setAction(Action::INDEX)
+                ->generateUrl()
+        );
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_OK);
+
+        $client->request(
+            "GET",
+            $adminUrlGenerator
+                ->setController(MemberCrudController::class)
+                ->setAction(Action::NEW)
+                ->generateUrl()
+        );
+
+        $this->assertResponseIsSuccessful();
+
+        $client->submitForm("Créer", [
+            "Member[name]" => "Raison sociale",
+            "Member[companyNumber]" => "44306184100047",
+            "Member[organization]" => 1
+        ]);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        $client->request(
+            "GET",
+            $adminUrlGenerator
+                ->setController(MemberCrudController::class)
+                ->setAction(Action::EDIT)
+                ->setEntityId(2)
+                ->generateUrl()
+        );
+
+        $this->assertResponseIsSuccessful();
+
+        $client->submitForm("Sauvegarder les modifications", [
+            "Member[name]" => "Raison sociale",
+            "Member[companyNumber]" => "42878504200105",
+            "Member[organization]" => 1
+        ]);
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        /** @var Member $member */
+        $member = $entityManager->getRepository(Member::class)->findOneByName("Raison sociale");
+
+        $client->request(
+            "GET",
+            $adminUrlGenerator
+                ->setController(MemberCrudController::class)
+                ->setAction(Action::DELETE)
+                ->setEntityId($member->getId())
+                ->generateUrl()
+        );
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        $client->request(
+            "GET",
+            $adminUrlGenerator
+                ->setController(MemberCrudController::class)
+                ->setAction(Action::DETAIL)
+                ->setEntityId(2)
+                ->generateUrl()
+        );
+
+        $this->assertResponseIsSuccessful();
+    }
+}
