a42d805ecfb410db055dc08762fcd886b900d812
TBoileau <t-boileau@email.com>
2021-03-21 12:49:05 +0100

    fix: delete rules from FAQ (#132)

--- src/Controller/FaqController.php
+++ src/Controller/FaqController.php
@@ -5,9 +5,7 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use App\Entity\Question;
-use App\Entity\Rules;
 use App\Repository\QuestionRepository;
-use App\Repository\RulesRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,13 +17,11 @@ class FaqController extends AbstractController
 {
     /**
      * @param QuestionRepository<Question> $questionRepository
-     * @param RulesRepository<Rules> $rulesRepository
      */
-    public function __invoke(QuestionRepository $questionRepository, RulesRepository $rulesRepository): Response
+    public function __invoke(QuestionRepository $questionRepository): Response
     {
         return $this->render("ui/faq.html.twig", [
-            "questions" => $questionRepository->findAll(),
-            "rules" => $rulesRepository->getLastPublishedRules()
+            "questions" => $questionRepository->findAll()
         ]);
     }
 }
