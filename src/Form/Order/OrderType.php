a74e92139bd1ad1e90625bea536d0018f917ef69
TBoileau <t-boileau@email.com>
2021-09-14 19:23:09 +0200

    feat: select order delivery address (#227)

--- src/Form/Order/OrderType.php
+++ src/Form/Order/OrderType.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace App\Form\Order;
 
+use App\Entity\Address;
 use App\Entity\Order\Order;
 use App\Entity\User\Customer;
 use App\Form\AddressType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormEvent;
@@ -23,13 +25,22 @@ class OrderType extends AbstractType
 
             $form = $event->getForm();
 
-            if ($order->getUser() instanceof Customer) {
-                /** @var Customer $customer */
-                $customer = $order->getUser();
-
-                if ($customer->isManualDelivery()) {
-                    $form->add("deliveryAddress", AddressType::class, ["validation_groups" => ["Default", "order"]]);
-                }
+            if (
+                !$order->getUser() instanceof Customer
+                || ($order->getUser() instanceof Customer && $order->getUser()->isManualDelivery())
+            ) {
+                $form->add("deliveryAddress", EntityType::class, [
+                    'label' => 'Adresse de livraison',
+                    "class" => Address::class,
+                    "choice_label" => fn (Address $address) => sprintf(
+                        "%s - %s %s %s",
+                        $address->getFullName(),
+                        $address->getStreetAddress(),
+                        $address->getLocality(),
+                        $address->getLocality()
+                    ),
+                    "choices" => $order->getUser()->getDeliveryAddresses()
+                ]);
             }
         });
     }
