2303ad2d5909307509ffdd3b2bf80a62d873ce62
Thomas Boileau <t-boileau@email.com>
2021-10-21 09:42:36 +0200

    Orders (#289)
    * feat: table ux (#250)
    
    * feat: sort and filter client orders (#288)

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -13,6 +13,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\BinaryFileResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -26,13 +27,39 @@ class OrderController extends AbstractController
      * @param OrderRepository<Order> $orderRepository
      * @Route("/", name="order_index")
      */
-    public function index(OrderRepository $orderRepository): Response
+    public function index(OrderRepository $orderRepository, Request $request): Response
     {
         /** @var User $user */
         $user = $this->getUser();
 
+        $clientOrders = [];
+
+        if ($this->isGranted('ROLE_SALES_PERSON') || $this->isGranted('ROLE_MANAGER')) {
+            if ($request->query->get("field") === null) {
+                $request->query->set("field", 'o.createdAt');
+            }
+
+            if ($request->query->get("direction") === null) {
+                $request->query->set("direction", 'desc');
+            }
+
+            /** @var SalesPerson|Manager $employee */
+            $employee = $user;
+
+            $clientOrders = $orderRepository->getOrdersByMemberEmployee(
+                $employee,
+                $request->query->getInt("page", 1),
+                10,
+                $request->query->get("field"),
+                $request->query->get("direction"),
+                $request->query->get("filter")
+            );
+        }
+
         return $this->render("ui/order/index.html.twig", [
-            "orders" => $orderRepository->findBy(["user" => $user], ["createdAt" => "desc"])
+            "orders" => $orderRepository->findBy(["user" => $user], ["createdAt" => "desc"]),
+            'clientOrders' => $clientOrders,
+            "pages" => ceil(count($clientOrders) / 10),
         ]);
     }
 
@@ -42,21 +69,6 @@ class OrderController extends AbstractController
     public function download(Order $order, string $publicDir): BinaryFileResponse
     {
         return $this->file(sprintf('%s/pdf/%s.pdf', $publicDir, $order->getReference()));
-    }
-
-    /**
-     * @param OrderRepository<Order> $orderRepository
-     * @Route("/clients", name="order_clients")
-     * @Security("is_granted('ROLE_SALES_PERSON') or is_granted('ROLE_MANAGER')")
-     */
-    public function clients(OrderRepository $orderRepository): Response
-    {
-        /** @var SalesPerson|Manager $user */
-        $user = $this->getUser();
-
-        return $this->render("ui/order/_clients.html.twig", [
-            "orders" => $orderRepository->getOrdersByMemberEmployee($user)
-        ]);
     }
 
     /**
